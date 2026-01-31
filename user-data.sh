f016f09359a3e32fb5bccdce8abf4ea7d14e34e9
Sneha1610 <snehalohakare20@gmail.com>
2026-01-31 19:54:03 +0530

    Add user-data script for Flask EC2

---
+++ user-data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dnf update -y
+dnf install docker -y
+
+systemctl start docker
+systemctl enable docker
+
+usermod -aG docker ec2-user
+
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+-o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
