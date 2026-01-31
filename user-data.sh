5dcd29215713bb5c80db69e146bbcb281a6e3256
Sneha1610 <snehalohakare20@gmail.com>
2026-01-31 20:57:30 +0530

    Fix user-data install docker awscli and compose

--- user-data.sh
+++ user-data.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 dnf update -y
 
-dnf install docker -y
-dnf install awscli -y
+dnf install -y docker awscli
 
 systemctl start docker
 systemctl enable docker
 
 usermod -aG docker ec2-user
 
-curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
--o /usr/local/bin/docker-compose
+mkdir -p /usr/local/lib/docker/cli-plugins
 
-chmod +x /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 \
+-o /usr/local/lib/docker/cli-plugins/docker-compose
+
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
